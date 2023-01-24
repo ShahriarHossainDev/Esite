e8bf22e93a7ea320acf7498149982a804dadb845
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-01-24 18:30:29 +0600

    S

--- Shop/models.py
+++ Shop/models.py
@@ -19,3 +19,4 @@ class userProfile(models.Model):
 
     def __str__(self):
         return str(self.user)
+
--- Shop/views.py
+++ Shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
+from .models import userProfile
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -74,5 +76,10 @@ def logout(request):
     return redirect('login')
 
 
+#@login_required()
 def profile(request):
-    return render(request, 'profile.html')
+    user = userProfile.objects.all()
+    Context = ({
+        "user": user
+    })
+    return render(request, 'profile.html', Context)
